4a3d82529c5855bed27e9436f9aeab111c962750
Vlassis Skandalis <random@skandalisvlassis.com>
2023-10-25 12:43:26 +0300

    Fixes logic

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const newNoteTitleInput = document.getElementById('new-note-title')
 const addNewButton = document.getElementById('add-new')
-const notesList = document.getElementById("notes-list");
+const notesList = document.getElementById("notes-list")
 const textArea = document.querySelector('textarea')
+let currentNoteId = null
+const notes = JSON.parse(localStorage.getItem("local-notes")) || {};
 
-addNewButton.addEventListener('click', () => { 
+addNewButton.addEventListener('click', (e) => { 	
+	e.stopPropagation()
+	e.preventDefault()
 	if (newNoteTitleInput.value.length <= 0) return
 
 	const tempItemId = generateUniqueID()
@@ -13,38 +17,55 @@ addNewButton.addEventListener('click', () => {
 
 	notesList.appendChild(listItem);
 
-	addNewButton.removeEventListener('click', clickHandler);
-
-	function clickHandler() {
-		openNote(tempItemId);
+	notes[tempItemId] = {
+		id: tempItemId,
+		title: listItem.textContent,
+		content: ''
 	}
 
-	addNewButton.addEventListener('click', clickHandler);
+	listItem.addEventListener('click', () => { openNote(tempItemId) });
 	openNote(tempItemId)
 
 	newNoteTitleInput.value = ''
+	textArea.focus()
 })
 
 
+textArea.addEventListener('input', () => { handleTextAreaInput() })
+
 function openNote(noteId) {
 	if (!noteId) return
-	textArea.value = localStorage.getItem(noteId)
-
-	textArea.removeEventListener('input', inputHandler)
-
-	function inputHandler() {
-		handleTextAreaInput(noteId)
-	}
-	textArea.addEventListener('input', inputHandler);
-
+	currentNoteId = noteId
+	
+	textArea.value = notes[currentNoteId]?.content || ''
+	
 	textArea.style.display = 'block'
 }
 
-function handleTextAreaInput(noteId) {
-	console.log(noteId);
-	localStorage.setItem(noteId, textArea.value)
+function handleTextAreaInput() {
+	console.log(notes);
+	console.log(notes[currentNoteId]?.content);
+	
+	notes[currentNoteId].content = textArea.value
+
+	localStorage.setItem("local-notes", JSON.stringify(notes));
 }
 
 function generateUniqueID() {
 	return Date.now() + "-" + Math.floor(Math.random() * 1000);
 }
+
+function populateNotesList() { 
+	if (notes.length == 0 || notes === null) return
+	
+	for (const [id, note] of Object.entries(notes)) {	
+		const listItem = document.createElement("li")
+		listItem.textContent = note.title
+		listItem.dataset.id = id
+		listItem.addEventListener('click', () => { openNote(id) });
+
+		notesList.appendChild(listItem)
+	}
+}
+
+populateNotesList()
